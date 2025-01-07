5d2caac9f6f0d9e3bcc778e175ee55a0200a9dcb
Poojitharedd-ops <poojithadspr@gmail.com>
2025-01-07 18:12:34 +0530

    Update kubectlinstall.sh

--- installkubectl/kubectlinstall.sh
+++ installkubectl/kubectlinstall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.6/2024-11-15/bin/linux/amd64/kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.6/2024-11-15/bin/linux/amd64/kubectl.sha256
 sha256sum -c kubectl.sha256
